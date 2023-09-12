bde074810ab5aaabd60e6f6c59848d7f62947354
G3R4RD0 <gerardo.vasquez.p@gmail.com>
2023-09-12 22:35:39 +0200

    training implemented for test chatbot

--- NLP-component/preprocessing.py
+++ NLP-component/preprocessing.py
@@ -1,6 +1,25 @@
 import spacy
+import numpy as np
 # load pre trained tokenization spanish model
-nlp = spacy.load("es_core_news_sm")
+# nlp = spacy.load("es_core_news_sm")
+
+# load pre trained tokenization English model
+nlp = spacy.load("en_core_web_sm")
+
+
+# def tokenize(sentence):
+
+#     doc = nlp(sentence)
+
+#     tokens = []
+#     for token in doc:
+#         tokens.append(token.text)
+
+#     return tokens
+
+
+# def lemmatize(token):
+#     return token.lemma_.lower()
 
 
 def preprocess_text(sentence):
@@ -15,14 +34,23 @@ def preprocess_text(sentence):
         lemma = token.lemma_.lower()
         tokens.append(lemma)
 
-    return tokens
+    filtered_tokens = [token for token in tokens if not nlp(token)[0].is_punct]
+
+    return filtered_tokens
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
 
-
-a = "hola, me gusta comer pan"
-print(a)
-a = preprocess_text(a)
-print(a)
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
